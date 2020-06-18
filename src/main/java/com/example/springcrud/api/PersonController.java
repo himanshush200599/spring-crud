188038a2e2ff85431a0fce07e283d72f1c3b2182
Himanshu Sharma <himanshush214@gmail.com>
2020-06-19 01:34:48 +0530

    Added GET method to fetch all the person

--- src/main/java/com/example/springcrud/api/PersonController.java
+++ src/main/java/com/example/springcrud/api/PersonController.java
@@ -3,13 +3,12 @@ package com.example.springcrud.api;
 import com.example.springcrud.model.Person;
 import com.example.springcrud.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
 @RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
     private final PersonService personService;
 
@@ -20,5 +19,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
     }
 }
