4b404e37480ad0beda8da8a29aadd151827b6cd6
Himanshu Sharma <himanshush214@gmail.com>
2020-06-18 22:33:42 +0530

    Spring boot Set up

---
+++ src/main/java/com/example/springcrud/SpringCrudApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from spring boot");
+        SpringApplication.run(SpringCrudApplication.class, args);
+    }
+
+}
